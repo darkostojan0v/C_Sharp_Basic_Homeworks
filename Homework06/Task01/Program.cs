36ffc1ecd52e6650aea7c7a079d5ad4171f2dc9d
darkostojan0v <95142705+darkostojan0v@users.noreply.github.com>
2022-04-10 20:02:07 +0200

    Homework06

---
+++ Homework06/Task01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Give the user an option to input 5 numbers
+            //Store all numbers in a QUEUE
+            //When the user is done adding numbers print the number in the order that the user entered them from the QUEUE
+
+            Queue<string> userInput = new Queue<string>();
+
+            Console.WriteLine("Please enter 5 numbers separated by a comma.");
+
+            userInput.Enqueue(Console.ReadLine());
+
+            foreach(var item in userInput)
+            {
+                Console.WriteLine($"The 5 numbers you entered in order are: {item}");
+            }
+            
+        }
+    }
+}
