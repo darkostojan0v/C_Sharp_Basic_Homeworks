a097d352b3f7f8e94e7cab1edcfbc6a8543ba2fe
darkostojan0v <95142705+darkostojan0v@users.noreply.github.com>
2022-04-10 19:55:06 +0200

    Homework06

---
+++ Homework06/Homework06/Homework06/Task03/Animal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task03
+{
+    class Animal
+    {
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public int Age { get; set; }
+
+        public GenderEnum Gender { get; set; }
+       
+    }
+}
